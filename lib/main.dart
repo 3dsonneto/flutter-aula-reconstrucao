b09080bcb2e707449c5b018e972cf986245688fa
Edson <edson_neto1996@hotmail.com>
2021-04-01 19:16:26 -0300

    Implementando Scaffold e criando a CounterScreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Container(
-        color: Colors.red,
-      ),
+      home: CounterScreen(),
     ),
   );
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Contador"),
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+      // bottomNavigationBar: Container(
+      //   height: 120,
+      //   color: Colors.red,
+      // ),
+    );
+  }
 }
