9fd9395ccdcf4648b4e5d9497cceb3931f57229f
Edson <edson_neto1996@hotmail.com>
2021-04-01 19:35:44 -0300

    Implementando o setState

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,44 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  var resultado = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Meu Contador"),
       ),
-      body: Container(
-        color: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Você pressionou o botão: ",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Center(
+            child: Text(
+              "$resultado",
+              style: TextStyle(fontSize: 34),
+            ),
+          ),
+        ],
       ),
       // bottomNavigationBar: Container(
       //   height: 120,
       //   color: Colors.red,
       // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Botão Pressionado");
+          setState(() {
+            resultado += 1;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
